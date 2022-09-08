30d09652b5f1df5ce04f05c5343d2b380eb46dfc
Thorben Muhleder <thorbenm@me.com>
2022-09-08 18:52:04 +0200

    split up scripts

---
+++ record_and_transfer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+filename=$1
+iptv_url=$2
+duration=$3
+password_path=/home/pi/Programming/iptv_recorder/tv_password
+
+echo "" >> /home/pi/iptv_recorder_logs.txt
+echo "$filename" >> /home/pi/iptv_recorder_logs.txt
+timeout $duration cvlc $iptv_url --sout=file/ts:/home/pi/recordings/$filename >> /home/pi/iptv_recorder_logs.txt 2>&1
+
+for j in 1 2 3; do
+    sshpass -p$(cat $password_path) scp /home/pi/recordings/$filename tv@tv:/Volumes/Torrents/_recordings
+    if sshpass -p$(cat $password_path) ssh tv@tv -t "ls /Volumes/Torrents/_recordings" | grep "$filename"; then
+	    rm /home/pi/recordings/$filename
+	    exit 0
+    fi
+done
+
+exit 2
---
+++ schedule_recording.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+channel=$1
+start=$2
+end=$3
+name=$4
+
+if [ "$(grep "\"$channel\"" /home/pi/iptv/* -h | wc -l)" != "1" ]; then echo "channel not found"; exit 2; fi
+
+filename=$(date -f - +%Y-%m-%d__%H-%M <<< "$start")__$(date -f - +%H-%M <<< "$end")__${channel// /_}__${name// /_}.ts
+iptv_url=$(grep "\"$channel\"" /home/pi/iptv/* -A 1 -h | tail -1)
+duration=$(( $(date -f - +%s- <<< "$end"$'\n'"$start") 0 ))
+
+echo "filename" $filename
+echo "iptv url" $iptv_url
+echo "duration" $duration "seconds"
+echo "duration" $(($duration / 60)) "minutes"
+echo "duration" $(($duration / 3600)) "hours"
+
+echo "record_and_transfer \"$filename\" \"$iptv_url\" \"$duration\"" | at $start
