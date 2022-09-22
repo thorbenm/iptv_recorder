6fefdd2eff4fc636dee7b0b321fbe91bfafbc24f
Thorben Muhleder <thorbenm@me.com>
2022-09-22 10:24:58 +0200

    add checkups for duration in schedule

--- schedule_recording.sh
+++ schedule_recording.sh
@@ -14,6 +14,21 @@ filename=$(date -f - +%Y-%m-%d__%H-%M <<< "$start")__$(date -f - +%H-%M <<< "$en
 iptv_url=$(grep "\"$channel\"" /home/pi/iptv/* -A 1 -h | tail -1)
 duration=$(( $(date -f - +%s- <<< "$end"$'\n'"$start") 0 ))
 
+if (( $duration < 0 )); then
+	echo "duration negative"
+	exit 2
+fi
+
+if (( 4 < $(($duration / 3600)) )); then
+	echo "warning: duration" $(($duration / 3600)) "hours"
+	echo ""
+fi
+
+if (( 10 < $(($duration / 3600)) )); then
+	echo "error: duration" $(($duration / 3600)) "hours"
+	exit 2
+fi
+
 echo "filename" $filename
 echo "iptv url" $iptv_url
 echo "duration" $duration "seconds"
