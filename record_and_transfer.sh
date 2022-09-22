52cd5e9fbd3aabc893796c26756e7217261c9a09
Thorben Muhleder <thorbenm@me.com>
2022-09-22 10:20:35 +0200

    ping tv before trying to tranfer
    something is wrong with my macbook and ssh doesnt work on first try.
    very ugly solution but works

--- record_and_transfer.sh
+++ record_and_transfer.sh
@@ -9,6 +9,13 @@ echo "" >> /home/pi/iptv_recorder_logs.txt
 echo "$filename" >> /home/pi/iptv_recorder_logs.txt
 timeout $duration cvlc $iptv_url --sout=file/ts:/home/pi/recordings/$filename >> /home/pi/iptv_recorder_logs.txt 2>&1
 
+ping tv -c 1
+ping tv -c 1
+ping tv -c 1
+sshpass -p$(cat $password_path) ssh tv@tv -t "ls ~"
+sshpass -p$(cat $password_path) ssh tv@tv -t "ls ~"
+sshpass -p$(cat $password_path) ssh tv@tv -t "ls ~"
+
 for j in 1 2 3; do
     sshpass -p$(cat $password_path) scp /home/pi/recordings/$filename tv@tv:/Volumes/Torrents/_recordings
     if sshpass -p$(cat $password_path) ssh tv@tv -t "ls /Volumes/Torrents/_recordings" | grep "$filename"; then
